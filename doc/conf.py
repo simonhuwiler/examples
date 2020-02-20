a2ae5ce44a33d5858c29ccc015fb155c13f47b9a
Jean-Luc Stevens <jlrstevens@gmail.com>
2020-02-20 15:39:14 -0600

    Change to be reverted after testing on Travis

--- doc/conf.py
+++ doc/conf.py
@@ -62,8 +62,12 @@ DIR = os.getenv('DIR')
 if DIR:
     projects = [DIR]
 else:
+    print("TRAVIS DEBUG:")
+    for root, dirs, files in os.walk("."):
+        print(":", root, dirs, files)
+
     projects = sorted([f for f in next(os.walk('.'))[1] if f not in EXCLUDE])
-    print('PROJECTS:', projects)
+    print('PROJECTS::', projects)
 
 nbsite_gallery_conf = {
     'host': 'assets',
